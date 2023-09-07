7941e7fb5a5df6667cdf675f98b42603d7197046
BetoPS93 <adalbertops93@live.com>
2023-09-07 19:39:34 -0300

    aula05

---
+++ conta02.py
@@ -0,0 +1,24 @@
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite) -> None:
+        print(f"Construindo objeto...{self}")
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+        
+    def extrato(self):
+        print(f"Saldo {self.__saldo} do titular {self.__titular}")
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
